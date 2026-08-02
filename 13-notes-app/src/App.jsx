7ee57a6d18316948d06f18e3499b98b8fa8674b7
firozansarjs <firoz.ansari.dev@gmail.com>
2026-08-03 00:37:08 +0530

    13-notes-project-complete

--- 13-notes-app/src/App.jsx
+++ 13-notes-app/src/App.jsx
@@ -4,10 +4,20 @@ const App = () => {
 const [title, setTitle] = useState('')
 const [details, setDetails] = useState('')
 
+const [task, setTask] = useState([])
+
 const newValue = (e) => {
    e.preventDefault(e)
+   const copyTask = [...task]
+   copyTask.push({title,details})
+   setTask(copyTask)
    setTitle('')
    setDetails('')
+}
+const deleteNote = (idx) => {
+   const copyTask = [...task]
+   copyTask.splice(idx,1)
+   setTask(copyTask)
 }
 
   return (
@@ -15,6 +25,7 @@ const newValue = (e) => {
       <form className='flex flex-col items-start lg:w-1/2  gap-5 text-white p-10'  onSubmit={(e) => {
           newValue(e)
     }}>
+      <h1 className='font-bold text-2xl'>Add notes</h1>
       <input 
       className='px-5 py-2 w-1/2 border-2 rounded outline-none font-bold '
       type="text" 
@@ -33,15 +44,24 @@ const newValue = (e) => {
         }}
       >
 </textarea>
-      <button className='bg-white text-black h-10 w-1/2 px-5 py-2 rounded outline-none' onClick={newValue}>
-      Add notes</button>
+      <button className='bg-white text-black h-10 w-1/2 px-5 py-2 rounded outline-none active:scale-95' onClick={newValue}>
+      Add notes
+      </button>
     </form>
-    <div className=' lg:w-1/2 border-l-10 p-10'>
-      <h1 className='text-xl font-bold text-white'>Your notes</h1>
-    <div className='flex flex-wrap  gap-5 mt-5'>
-      <div className='h-50 w-40 rounded-2xl bg-amber-50'></div>
-    <div className='h-50 w-40 rounded-3xl bg-amber-50'></div>
-    <div className='h-50 w-40 rounded-3xl bg-amber-50'></div>
+    <div className="lg:w-1/2 lg:border-l-2 lg:border-white p-10">
+      <h1 className='text-2xl font-bold text-white'>Recent notes</h1>
+    <div className='flex flex-wrap text-black gap-5 mt-5'>
+        {task.map(function(elem,idx){
+           return <div key={idx} className='relative flex justify-between items-start flex-col h-50 w-40 rounded-2xl p-5 bg-cover bg-[url(https://imgs.search.brave.com/gI2GCJg-U-FDWph_skhDA3Rl8m7XrJt0a83IPNfWgfA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wNjYv/NDI3LzQwOS9zbWFs/bC9hLXBpZWNlLW9m/LXBhcGVyLXdpdGgt/YS1yZWQtc3RpY2t5/LW5vdGUtcG5nLnBu/Zw)]'>
+            <div>
+            <h1 className=' font-bold text-2xl leading-tight'>{elem.title}</h1>
+            <p className='mt-2 leading-tight font-medium text-gray-600'>{elem.details}</p>
+            </div>
+             <button className='bg-red-500 cursor-pointer active:scale-95 rounded w-full py-1 text-white font-bold text-xs' onClick={() => {
+              deleteNote(idx)
+             }}>Delete note</button>
+           </div>
+        })}
     </div>
     </div>
     </div>
