5ab7c442751c2840b15b5251d6008f7deb086745
firozansarjs <firoz.ansari.dev@gmail.com>
2026-08-03 13:49:02 +0530

    add some new file

--- 15-api-calling/src/App.jsx
+++ 15-api-calling/src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
 
 const App = () => {
+  const [data, setData] = useState([])
+  const getData = async () => {
+    const {data} = await axios.get('https://picsum.photos/v2/list')
+    setData(data)
+  }
 
   return (
     <div>
-      <button onClick={}>Get data</button>
+      <button onClick={getData}>Get data</button>
+      <div>
+        {data.map(function(elem,idx){
+          return <h3>hello {elem.author} {idx}</h3>
+        })}
       </div>
+      </div>   
   )
 }
 
