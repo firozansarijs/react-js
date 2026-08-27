9870517e33b5379b013bcf006316a88fd99fe956
firozansarjs <firoz.ansari.dev@gmail.com>
2026-08-27 22:09:49 +0530

    final-project

--- 20-Final-boss/src/components/Category.jsx
+++ 20-Final-boss/src/components/Category.jsx
@@ -6,11 +6,10 @@ const Category = () => {
         <div>
        <p className='text-amber-600 px-5 py-10 flex justify-center items-center text-xs'>E X P L O R E</p>
        <br />
-       <h1 className='text-black font-style: italic flex justify-center items-center text-5xl'>Shop By Category</h1>
+       <h1 className='text-black font-style: italic flex justify-center text-5xl'>Shop By Category</h1>
        <br />
-       <p></p>
+       <p className='text-gray-400 flex justify-center'>Find your perfect style across our thoughtfully curated collections</p>
         </div>
-       
     </div>
   )
 }
